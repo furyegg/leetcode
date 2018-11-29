51db91118e0841c6cbe3a80c55eac5a98c9f8165
lyhtbc <lyhtbc@gmail.com>
2018-11-30 00:14:21 +0800

    code

---
+++ src/main/scala/com/leetcode/easy/TwoSum.scala
@@ -0,0 +1,11 @@
+package com.leetcode.easy
+
+object Solution {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    return Array()
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println("abc")
+  }
+}

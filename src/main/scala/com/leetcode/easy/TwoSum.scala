3c8c5a0b987f064af1b32c361aa0f20407ef1bed
lyhtbc <lyhtbc@gmail.com>
2018-11-30 08:30:48 +0800

    twosum

--- src/main/scala/com/leetcode/easy/TwoSum.scala
+++ src/main/scala/com/leetcode/easy/TwoSum.scala
@@ -1,11 +1,26 @@
 package com.leetcode.easy
 
 object Solution {
+  
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    return Array()
+    val indexedNums = nums.zipWithIndex.sortBy(t => t._1)
+    for (i <- 0 until indexedNums.size) {
+      val res = find(nums, i, target)
+      if (res.isDefined) return Array(i, res.get)
+    }
+    Array()
+  }
+  
+  def find(nums: Array[Int], i: Int, target: Int): Option[Int] = {
+  
+  }
+  
+  def doFind(nums: Array[Int], start: Int, end: Int, i: Int, target: Int, p: (Int, Int) => Boolean) {
+  
   }
   
   def main(args: Array[String]): Unit = {
-    println("abc")
+    val nums = Array(2, 1, 7)
+    twoSum(nums, 9)
   }
 }
